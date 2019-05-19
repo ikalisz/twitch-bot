1849e8015939cb8bed4e675940875d4ce74ac62f
Ian Kaliszewski <ikalisz@yahoo.com>
2019-05-18 19:38:54 -0600

    set up the opts kind of

---
+++ index.js
@@ -0,0 +1,13 @@
+const tmi = require('tmi.js')
+
+//define config opts
+
+const opts = {
+    identity: {
+        username: 'username here',
+        password: 'auth token here'
+    },
+    channels: [
+        'Channel_name here'
+    ]
+}
